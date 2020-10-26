9a82d78832d09a527965714170685b1b4456cc52
Ricardo Ramírez <rickyrb@gmail.com>
2020-10-26 18:50:10 -0500

    * Más ejemplos en Union

--- src/main/java/com/sophos/reactor/Union.java
+++ src/main/java/com/sophos/reactor/Union.java
@@ -1,13 +1,34 @@
 package com.sophos.reactor;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
-import java.util.concurrent.CountDownLatch;
 
 public class Union {
 
+	/** Flujos para pruebas */
+	private static final Flux<Integer> pares = Flux.just(2, 4, 6, 8, 10).delayElements(Duration.ofMillis(10));
+	private static final Flux<Integer> impares = Flux.just(1, 3, 5, 7, 9).delayElements(Duration.ofMillis(20));
+
+
 	public static void main(String[] args) {
+		zip();
+		concat();
+		merge();
+		mergeSequential();
+		mergeOrdered();
+		switchIfEmpty();
+		switchMap();
+
+		try {
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void zip() {
 		// Zip
 		Flux.just("Una frase muy larga para enumerar y contar la cantidad de palabras")
 			.flatMap(frase -> Flux.fromArray(frase.split(" ")))
@@ -21,33 +42,54 @@ public class Union {
 			.subscribe(cantidad -> System.out.println("Se contaron " + cantidad + " palabras en el Flux"))
 		;
 		System.out.println();
+	}
 
-		// Flujos para pruebas
-		Flux<Integer> pares = Flux.just(2, 4, 6, 8, 10).delayElements(Duration.ofMillis(10));
-		Flux<Integer> impares = Flux.just(1, 3, 5, 7, 9).delayElements(Duration.ofMillis(20));
-
-		// Concat
-//		pares.concatWith(impares).subscribe(valor -> System.out.println("Concat - " + valor));
+	private static void concat() {
+		pares.concatWith(impares)
+			.subscribe(valor -> System.out.println("Concat - " + valor))
+		;
 		System.out.println();
+	}
 
-		// Merge
-//		Flux.merge(pares, impares).subscribe(valor -> System.out.println("Merge - " + valor));
+	private static void merge() {
+		Flux.merge(pares, impares)
+			.subscribe(valor -> System.out.println("Merge - " + valor))
+		;
 		System.out.println();
+	}
 
-		// Merge secuencial
-//		Flux.mergeSequential(pares, impares).subscribe(valor -> System.out.println("MergeSequential - " + valor));
+	private static void mergeSequential() {
+		Flux.mergeSequential(pares, impares)
+			.subscribe(valor -> System.out.println("MergeSequential - " + valor));
 		System.out.println();
+	}
 
-		// Merge ordenado
-		Flux.mergeOrdered(pares, impares).subscribe(valor -> System.out.println("MergeOrdered - " + valor));
+	private static void mergeOrdered() {
+		Flux.mergeOrdered(pares, impares)
+			.subscribe(valor -> System.out.println("MergeOrdered - " + valor));
 		System.out.println();
+	}
 
+	private static void switchIfEmpty() {
+		Flux.just(1, 3, 5, 7, 9)
+			.filter(i -> i % 2 == 0)
+			.map(String::valueOf)
+			.switchIfEmpty(Mono.just("Vacío"))
+			.subscribe(System.out::println)
+		;
+		System.out.println();
+	}
 
-		try {
-			Thread.sleep(3000);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	private static void switchMap() {
+		Flux.just(1, 2, 3, 4, 5)
+			.switchMap(valor ->
+				valor % 2 == 0
+					? Mono.just(0)
+					: Flux.just(1, 2)
+			)
+			.subscribe(System.out::println)
+		;
+		System.out.println();
 	}
 
 }
